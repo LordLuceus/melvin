00a9f5e5da07f4b9f114a2697704f76e7f1a0e31
LordLuceus <lord.luceus@gmail.com>
2021-07-04 12:38:24 +0200

    Make it possible to mix saved rolls with raw dice notation

--- src/util/roll-dice.ts
+++ src/util/roll-dice.ts
@@ -6,9 +6,7 @@ export const rollDice = (
   client: CommandoClient,
   msg: CommandoMessage
 ): DiceRoll => {
-  if (dice.startsWith("$")) {
-    const name = dice.replace("$", "");
-
+  if (dice.includes("$")) {
     const savedSettings = client.provider.get(msg.guild, "rolls");
     if (!savedSettings) {
       throw new Error(
@@ -28,11 +26,37 @@ export const rollDice = (
       );
     }
 
+    let name: string;
+    let rollString: string;
+
+    if (dice.startsWith("$")) {
+      if (!dice.includes(" ")) {
+        name = dice.replace("$", "");
+        rollString = parsedSettings[msg.author.id][name];
+      } else {
+        const args = dice.split(" ");
+        name = args[0].replace("$", "");
+        const after = args.slice(1).join("").trim();
+        rollString = parsedSettings[msg.author.id][name] + after;
+      }
+    } else {
+      const before = dice.slice(0, dice.indexOf("$")).trim();
+      name = dice.slice(dice.indexOf("$")).replace("$", "");
+
+      if (name.includes(" ")) {
+        const after = name.slice(name.indexOf(" ")).trim();
+        name = name.slice(0, name.indexOf(" "));
+        rollString = before + parsedSettings[msg.author.id][name] + after;
+      } else {
+        rollString = before + parsedSettings[msg.author.id][name];
+      }
+    }
+
     if (!parsedSettings[msg.author.id][name]) {
       throw new Error("No roll saved with that name.");
     }
 
-    return new DiceRoll(parsedSettings[msg.author.id][name]);
+    return new DiceRoll(rollString);
   }
 
   return new DiceRoll(dice);
