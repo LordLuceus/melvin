6818f8fbf3d833ab55e3a4ed95d660b9d88c2d58
LordLuceus <lord.luceus@gmail.com>
2022-07-01 16:30:28 +0200

    Add instruction to reinvite in the playing status

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -14,7 +14,8 @@ export class ReadyListener extends Listener {
     setEngine();
     this.container.client.user?.setActivity({
       type: ActivityTypes.PLAYING,
-      name: `havoc with ${this.container.client.guilds.cache.size} servers.`,
+      // name: `havoc with ${this.container.client.guilds.cache.size} servers.`,
+      name: "Reinvite me if I'm not working: https://discord.com/api/oauth2/authorize?client_id=813806889657434173&permissions=0&scope=bot%20applications.commands",
     });
     writeLog(LogLevel.Info, this.name, "Ready to roll!");
   }
